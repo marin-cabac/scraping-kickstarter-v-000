6e392c6fb8719f7ee99029d42afe1091b48777ef
Marin Cabac <marin.cabac@gmail.com>
2018-03-30 20:29:25 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,5 +4,5 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
-  project=project.css("h2.bbcard_name strong a").text
+  project=kickstarter.css("h2.bbcard_name strong a").text
 end
